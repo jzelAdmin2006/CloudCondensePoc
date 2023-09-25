4010e3cb66ddf45b7e03ae19a33237ea5e488efb
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-09-25 19:04:09 +0200

    correct naming

--- src/main/java/tech/bison/trainee/CloudCondensePocApplication.java
+++ src/main/java/tech/bison/trainee/CloudCondensePocApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudCondensePoc2Application {
+public class CloudCondensePocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudCondensePoc2Application.class, args);
+		SpringApplication.run(CloudCondensePocApplication.class, args);
 	}
 
 }
