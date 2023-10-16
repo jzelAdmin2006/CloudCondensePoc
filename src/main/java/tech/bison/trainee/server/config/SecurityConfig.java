b85e5a5d58e30fef704ed4c19b39bdb0a8c01bab
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-10-16 22:45:26 +0200

    simplify method reference

--- src/main/java/tech/bison/trainee/server/config/SecurityConfig.java
+++ src/main/java/tech/bison/trainee/server/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -18,18 +19,18 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults())
-        .csrf(csrf -> csrf.disable())
+        .csrf(CsrfConfigurer::disable)
         .authorizeHttpRequests(requests -> requests.requestMatchers("/**").permitAll());
     return http.build();
   }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
+    final CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList("*"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("content-type", "x-requested-with"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
   }
