0af8889eacb8de051c0a1716e70c688b0950d165
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-10-16 18:46:01 +0200

    dynamically display cloud storage types in frontend

--- src/main/java/tech/bison/trainee/server/webservice/adapter/rest/CloudStorageController.java
+++ src/main/java/tech/bison/trainee/server/webservice/adapter/rest/CloudStorageController.java
@@ -1,5 +1,6 @@
 package tech.bison.trainee.server.webservice.adapter.rest;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import tech.bison.trainee.server.business.service.CloudStorageService;
+import tech.bison.trainee.server.persistence.domain.cloud_storage.CloudStorageType;
 import tech.bison.trainee.server.webservice.adapter.model.cloud_storage.CloudStorageRequestDto;
 import tech.bison.trainee.server.webservice.adapter.model.cloud_storage.CloudStorageResourceDto;
 import tech.bison.trainee.server.webservice.service.WebMapperService;
@@ -32,5 +34,13 @@ public class CloudStorageController {
   @PostMapping
   public ResponseEntity<CloudStorageResourceDto> addEntry(@RequestBody CloudStorageRequestDto entry) {
     return ResponseEntity.ok(webMapperService.toDto(service.addCloudStorageEntry(webMapperService.fromDto(entry))));
+  }
+
+  @GetMapping("/types")
+  public ResponseEntity<List<String>> getCloudStorageTypes() {
+    return ResponseEntity.ok(Arrays.stream(CloudStorageType.values())
+        .filter(type -> type != CloudStorageType.UNKNOWN)
+        .map(CloudStorageType::getDisplayName)
+        .toList());
   }
 }
