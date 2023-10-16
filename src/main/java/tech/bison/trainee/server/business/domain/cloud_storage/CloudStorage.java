f95827a2afb89d351f294824443a0b759d28f221
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-10-16 20:00:12 +0200

    reformat code

--- src/main/java/tech/bison/trainee/server/business/domain/cloud_storage/CloudStorage.java
+++ src/main/java/tech/bison/trainee/server/business/domain/cloud_storage/CloudStorage.java
@@ -4,6 +4,5 @@ import java.util.Date;
 
 import tech.bison.trainee.server.persistence.domain.cloud_storage.CloudStorageType;
 
-public record CloudStorage(int id, String name, CloudStorageType type, String username, String password,
-    Date created) {
+public record CloudStorage(int id, String name, CloudStorageType type, String username, String password, Date created) {
 }
