1a48e367f994133e5557b8d4d6d19e1af1e6dcc9
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-10-17 18:12:52 +0200

    implement scheduled service and prepare further procedure

--- src/main/java/tech/bison/trainee/server/common/davresource/ResourceURL.java
+++ src/main/java/tech/bison/trainee/server/common/davresource/ResourceURL.java
@@ -12,14 +12,8 @@ public class ResourceURL {
   private final String baseUrl;
   private final DavResource resource;
 
-  public String toStringNoTrailingSlash() {
-    final HttpUrl baseHttpUrl = requireNonNull(HttpUrl.parse(baseUrl), "URL is invalid");
-    final String fullPath = resource.getHref().getPath();
-    final String resolvedUrl = baseHttpUrl.resolve(fullPath).toString();
-    return resolvedUrl.endsWith("/") ? resolvedUrl.substring(0, resolvedUrl.length() - 1) : resolvedUrl;
-  }
-
-  public String toStringTrailingSlash() {
-    return toStringNoTrailingSlash() + "/";
+  @Override
+  public String toString() {
+    return requireNonNull(HttpUrl.parse(baseUrl), "URL is invalid").resolve(resource.getHref().getPath()).toString();
   }
 }
